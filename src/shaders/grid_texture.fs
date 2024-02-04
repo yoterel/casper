fffb1c7be2402ce97c4efc1f8f370e7bc3f7cbba
yotam <erelyotam@gmail.com>
2024-02-04 19:58:58 +0900

    mls+game works

--- src/shaders/grid_texture.fs
+++ src/shaders/grid_texture.fs
@@ -10,7 +10,7 @@ void main()
     vec4 color = texture(src, TexCoord);
     // solve black fringe artifact due to alpha interpolation
     // note: hack. the best solution is alpha premultiplication
-    if(color.a < 1.0)
+    if(color.a < 0.8)
         discard;
     FragColor = color;
 }
